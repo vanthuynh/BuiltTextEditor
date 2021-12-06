b5e3091c314472deea36323a39ad830fb52aac0f
Van Huynh <vthuynh@cpp.edu>
2021-12-06 09:36:13 +0000

    display Welcome message in the center

--- kilo.c
+++ kilo.c
@@ -8,7 +8,8 @@
 #include <termios.h>
 #include <unistd.h>
 
-/***  includes ***/
+/***  defines ***/
+#define KILO_VERSION "0.0.1"
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 /***  data ***/
@@ -108,7 +109,21 @@ void abFree(struct abuf *ab) {
 void editorDrawRows(struct abuf *ab) {
     int y;
     for (y = 0; y < E.screenrows; y++) {
-        abAppend(ab, "~", 1);
+        if (y == E.screenrows / 3) {
+            char welcome[80];
+            int welcomelen = snprintf(welcome, sizeof(welcome),
+                "Kilo editor -- version %s", KILO_VERSION);
+            if (welcomelen > E.screencols) welcomelen = E.screencols;
+            int padding = (E.screencols - welcomelen) / 2; // divide the width by 2
+            if (padding) {
+                abAppend(ab, "~", 1);
+                padding--;
+            }
+            while (padding--) abAppend(ab, " ", 1); // subtract of string's length
+            abAppend(ab, welcome, welcomelen);
+        } else {
+            abAppend(ab, "~", 1);
+        }
 
         abAppend(ab, "\x1b[K", 3); // k command erases part of the current line
         if (y < E.screenrows - 1) {
