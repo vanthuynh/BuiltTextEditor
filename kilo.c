2461c230cf900d294fdd5d38e8eca17711b21772
Van Huynh <vthuynh@cpp.edu>
2021-12-06 09:08:03 +0000

    display window size - number of tildes

--- kilo.c
+++ kilo.c
@@ -3,6 +3,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
@@ -11,6 +12,8 @@
 
 /***  data ***/
 struct editorConfig {
+    int screenrows;
+    int screencols;
     struct termios orig_termios;
 };
 struct editorConfig E;
@@ -51,10 +54,22 @@ char editorReadKey() {
     }
     return c;
 }
+
+int getWindowSize(int *rows, int *cols) {
+    struct winsize ws;
+    // ioctl = Input/Output Control Get Window Size
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+        return -1;
+    } else {
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
+}
 /*** output ***/
 void editorDrawRows() {
     int y;
-    for (y = 0; y < 24; y++) {
+    for (y = 0; y < E.screenrows; y++) {
         write(STDOUT_FILENO, "~\r\n", 3);
     }
 }
@@ -75,10 +90,14 @@ void editorProcessKeypress() {
             break;
     }
 }
-/***  init ***/
+/*** init ***/
+void initEditor() {
+    // add screenrows and screencols to global editor state
+    if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+}
 int main() {
     enableRawMode();
-
+    initEditor();
     while (1) {
         editorRefreshScreen();
         editorProcessKeypress();
