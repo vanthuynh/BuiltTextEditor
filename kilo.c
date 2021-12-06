4ff0f5265b2122d6bd237362789f258f4e0d1804
Van Huynh <vthuynh@cpp.edu>
2021-12-06 09:27:59 +0000

    update the whole screen at once by append all write commands into buffer

--- kilo.c
+++ kilo.c
@@ -3,6 +3,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
@@ -83,21 +84,48 @@ int getWindowSize(int *rows, int *cols) {
         return 0;
     }
 }
+
+/*** append buffer ***/
+struct abuf {
+    char *b;
+    int len;
+};
+#define ABUF_INIT {NULL, 0}
+// constructor
+void abAppend(struct abuf *ab, const char *s, int len) {
+    char *new = realloc(ab->b, ab->len + len);
+    if (new == NULL) return;
+    memcpy(&new[ab->len], s, len);
+    ab->b = new;
+    ab->len += len;
+}
+// destructor
+void abFree(struct abuf *ab) {
+    free(ab->b);
+}
+
 /*** output ***/
-void editorDrawRows() {
+void editorDrawRows(struct abuf *ab) {
     int y;
     for (y = 0; y < E.screenrows; y++) {
-        write(STDOUT_FILENO, "~", 1);
+        abAppend(ab, "~", 1);
         if (y < E.screenrows - 1) {
-            write(STDOUT_FILENO, "\r\n", 2);
+            abAppend(ab, "\r\n", 2);
         }
     }
 }
 void editorRefreshScreen() {
-    write(STDOUT_FILENO, "\x1b[2J", 4); // write 4 bytes in the terminal
-    write(STDOUT_FILENO, "\x1b[H", 3);
-    editorDrawRows();
-    write(STDOUT_FILENO, "\x1b[H", 3);
+    struct abuf ab = ABUF_INIT; // initialize a new abuf
+
+    abAppend(&ab, "\x1b[2J", 4); // replace each occurence of write(...)
+    abAppend(&ab, "\x1b[H", 3);
+
+    editorDrawRows(&ab);
+
+    abAppend(&ab, "\x1b[H", 3);
+
+    write(STDOUT_FILENO, ab.b, ab.len); // write the buffer's contents out
+    abFree(&ab);
 }
 /*** input ***/
 void editorProcessKeypress() {
