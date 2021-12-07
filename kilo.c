764fecdda0151b579ba6c70729e69b2b07d6b8d1
Van Huynh <vthuynh@cpp.edu>
2021-12-07 08:08:09 +0000

    add filename variable and save the filename when the file is opened

--- kilo.c
+++ kilo.c
@@ -47,6 +47,7 @@ struct editorConfig {
     int screencols;
     int numrows;
     erow *row; // make erow an array of erow structs -> store multiple lines
+    char *filename; // store the file name to be displayed
     struct termios orig_termios;
 };
 struct editorConfig E;
@@ -205,6 +206,9 @@ void editorAppendRow(char *s, size_t len) {
 
 /*** file i/o ***/
 void editorOpen(char *filename) {
+    free(E.filename);
+    E.filename = strdup(filename); // save a copy of the filename when a file is opened
+
     FILE *fp = fopen(filename, "r");
     if (!fp) die("fopen");
 
@@ -409,6 +413,8 @@ void initEditor() {
     E.coloff = 0;
     E.numrows = 0;
     E.row = NULL;
+    E.filename = NULL;
+
     if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
     E.screenrows -= 1;
 }
