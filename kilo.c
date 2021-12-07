601a3f93a91d2ca1db2485aff80e0bc649ff1111
Van Huynh <vthuynh@cpp.edu>
2021-12-07 01:24:47 +0000

    update editorDrawRows() to use E.row[y] instead of E.row

--- kilo.c
+++ kilo.c
@@ -219,9 +219,9 @@ void editorDrawRows(struct abuf *ab) {
                 abAppend(ab, "~", 1);
             }
         } else {
-            int len = E.row.size;
+            int len = E.row[y].size;
             if (len > E.screencols) len = E.screencols;
-            abAppend(ab, E.row.chars, len);
+            abAppend(ab, E.row[y].chars, len);
         }
         abAppend(ab, "\x1b[K", 3); // k command erases part of the current line
         if (y < E.screenrows - 1) {
