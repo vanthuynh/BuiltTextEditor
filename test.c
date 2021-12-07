b36224f35214e751f6822f241a97e8b144a582dc
Van Huynh <vthuynh@cpp.edu>
2021-12-07 01:57:41 +0000

    reposition the cursor correctly Step 70

---
+++ test.c
@@ -0,0 +1,39 @@
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+In aliquet auctor arcu et dictum. Sed metus leo, blandit vitae maximus sit amet
+, feugiat non arcu. Pellentesque quis leo lorem. Ut in neque lorem. Sed sed urna 
+sodales lectus tristique aliquam ac at quam. Lorem ipsum dolor sit amet, consectetur
+adipiscing elit. Fusce sed augue venenatis, gravida sapien eget, convallis neque. 
+Sed vel feugiat justo. Morbi et consequat eros. Suspendisse lacinia malesuada 
+hendrerit. Nam convallis tristique efficitur.
+
+Mauris vitae neque tortor. Nam lobortis congue nunc, blandit egestas massa 
+volutpat et. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mi 
+diam, molestie non lorem eu, ultrices ornare ligula. Nulla facilisi. Integer maximus 
+mi id condimentum aliquet. Fusce molestie rhoncus orci, vestibulum gravida ex 
+venenatis tincidunt. Quisque sit amet nibh semper, varius ex a, ultrices velit. 
+Ut tempor metus sit amet justo scelerisque semper. Suspendisse potenti. 
+Aliquam iaculis purus id volutpat mattis.
+
+Vivamus in nisl nibh. Maecenas hendrerit magna arcu. Aenean mattis suscipit lectus 
+ac porta. Duis nec sapien cursus, vestibulum purus in, sagittis orci. Nam at aliquet
+ neque, ac convallis urna. Vestibulum hendrerit ex tellus, eu condimentum eros 
+ rhoncus a. Cras fermentum vestibulum nibh, sit amet cursus risus ultricies ac. 
+ Sed imperdiet tellus tortor, a dapibus metus tincidunt in. Sed malesuada bibendum
+  laoreet. Praesent consequat nisl non nulla imperdiet dapibus. Integer et aliquam 
+  orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur 
+  ridiculus mus. In imperdiet molestie bibendum. Aliquam non convallis enim, in 
+  bibendum justo. Sed et elit vitae erat imperdiet vehicula vel eu augue. 
+  Nullam aliquam justo ultricies, egestas arcu eleifend, faucibus nisl.
+Phasellus elementum quam ac enim suscipit fermentum. Donec eu pharetra ex. Morbi faucibus ullamcorper porta. Donec finibus felis nec arcu tristique, nec porta turpis eleifend. Aenean et est ut mauris molestie cursus. Aliquam venenatis sollicitudin orci ut consequat. In hac habitasse platea dictumst. Nulla at lectus quis dui posuere euismod. Ut pharetra imperdiet quam eu maximus. Nullam posuere augue nunc, sed commodo risus elementum pulvinar. Suspendisse eu maximus leo. Mauris eget ipsum non dui semper interdum. Pellentesque cursus mollis posuere. Ut accumsan volutpat porta. Nulla facilisi. Nunc sollicitudin non risus sed eleifend.
+
+Maecenas tincidunt augue at fermentum aliquet. Maecenas nec turpis diam. 
+Aenean pellentesque arcu facilisis risus eleifend, sed hendrerit arcu molestie. 
+Sed sodales nisl ac tincidunt accumsan. Aliquam congue dui quam, ut venenatis augue 
+laoreet a. Ut ultricies lorem vitae lacus accumsan, quis fringilla ante dapibus. 
+Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis 
+egestas. Fusce iaculis sodales rhoncus. Pellentesque consectetur gravida nulla. 
+Suspendisse mollis interdum pellentesque. Integer tortor ipsum, fermentum vitae 
+viverra sit amet, vehicula sed odio. Sed scelerisque urna nunc, eu convallis velit 
+vehicula vel. Phasellus efficitur felis quis tincidunt malesuada. Phasellus commodo 
+condimentum fringilla. Phasellus aliquet lectus eget quam hendrerit, sit amet 
+accumsan nulla mattis. Ut quis odio risus.
