0ae4db70d354ae85370f7aaf6c1422e377f2bfb7
Van Huynh <vthuynh@cpp.edu>
2021-12-07 02:15:05 +0000

    Snap cursor to end of line

--- kilo.c
+++ kilo.c
@@ -273,26 +273,31 @@ void editorMoveCursor(int key) {
     erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
 
     switch (key) {
-    case ARROW_LEFT:
-        if (E.cx != 0) {
-            E.cx--;
-        }
-        break;
-    case ARROW_RIGHT:
-        if (row && E.cx < row->size) {
-            E.cx++;
-        }
-        break;
-    case ARROW_UP:
-        if (E.cy != 0) {
-            E.cy--;
-        }
-        break;
-    case ARROW_DOWN:
-        if (E.cy < E.numrows) {
-            E.cy++;
-        }
-        break;
+        case ARROW_LEFT:
+            if (E.cx != 0) {
+                E.cx--;
+            }
+            break;
+        case ARROW_RIGHT:
+            if (row && E.cx < row->size) {
+                E.cx++;
+            }
+            break;
+        case ARROW_UP:
+            if (E.cy != 0) {
+                E.cy--;
+            }
+            break;
+        case ARROW_DOWN:
+            if (E.cy < E.numrows) {
+                E.cy++;
+            }
+            break;
+    }
+    row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+    int rowlen = row ? row->size : 0;
+    if (E.cx > rowlen) {
+        E.cx = rowlen;
     }
 }
 void editorProcessKeypress() {
