c9edf53d156b8792be38d5c4584c2c0cc29f5ccd
Van Huynh <vthuynh@cpp.edu>
2021-12-07 00:19:45 +0000

    prevent moving the cursor off screen

--- kilo.c
+++ kilo.c
@@ -172,18 +172,26 @@ void editorRefreshScreen() {
 /*** input ***/
 void editorMoveCursor(int key) {
     switch (key) {
-        case ARROW_LEFT:
+    case ARROW_LEFT:
+        if (E.cx != 0) {
             E.cx--;
-            break;
-        case ARROW_RIGHT:
+        }
+        break;
+    case ARROW_RIGHT:
+        if (E.cx != E.screencols - 1) {
             E.cx++;
-            break;
-        case ARROW_UP:
+        }
+        break;
+    case ARROW_UP:
+        if (E.cy != 0) {
             E.cy--;
-            break;
-        case ARROW_DOWN:
+        }
+        break;
+    case ARROW_DOWN:
+        if (E.cy != E.screenrows - 1) {
             E.cy++;
-            break;
+        }
+        break;
     }
 }
 void editorProcessKeypress() {
