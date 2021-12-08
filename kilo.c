bea0cae9c8aa664b8586572a0f69b65dcacea789
Van Huynh <vthuynh@cpp.edu>
2021-12-08 07:53:34 +0000

    add Quit Confirmation

--- kilo.c
+++ kilo.c
@@ -19,6 +19,7 @@
 /***  defines ***/
 #define KILO_VERSION "0.0.1"
 #define KILO_TAB_STOP 8
+#define KILO_QUIT_TIMES 3
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 enum editorKey {
@@ -468,12 +469,20 @@ void editorMoveCursor(int key) {
 }
 
 void editorProcessKeypress() {
+    static int quit_times = KILO_QUIT_TIMES;
     int c = editorReadKey();
     switch (c) {
         case '\r': // this is enter key
             /* TODO */
             break;
         case CTRL_KEY('q'):
+            // require user to press Ctrl-Q 3 times before quitting
+            if (E.dirty && quit_times > 0) {
+                editorSetStatusMessage("WARNING!!! File has unsaved changes. "
+                    "Press Ctrl-Q %d more times to quit.", quit_times);
+                quit_times--;
+                return;
+            }
             write(STDOUT_FILENO, "\x1b[2J", 4);
             write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
@@ -522,6 +531,7 @@ void editorProcessKeypress() {
             editorInsertChar(c);
             break;
     }
+    quit_times = KILO_QUIT_TIMES;
 }
 /*** init ***/
 void initEditor() {
