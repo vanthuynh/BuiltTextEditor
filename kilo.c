43dea9d241278d099e044cdf476f4d97287f9e8c
Van Huynh <vthuynh@cpp.edu>
2021-12-08 02:16:09 +0000

    implement function that insert ordinary characters

--- kilo.c
+++ kilo.c
@@ -208,6 +208,16 @@ void editorAppendRow(char *s, size_t len) {
     E.numrows++;
 }
 
+
+void editorRowInsertChar(erow *row, int at, int c) {
+    if (at < 0 || at > row->size) at = row->size; // validate at - index that will be inserted a character
+    row->chars = realloc(row->chars, row->size + 2);
+    memmove(&row->chars[at + 1], &row->chars[at], row->size - at + 1); // memove() = memcpy()
+    row->size++;
+    row->chars[at] = c;
+    editorUpdateRow(row);
+}
+
 /*** file i/o ***/
 void editorOpen(char *filename) {
     free(E.filename);
