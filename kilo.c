b7139542428c0623507ed64d9a065af75bc9d79f
Van Huynh <vthuynh@cpp.edu>
2021-12-08 08:10:41 +0000

    make editorDelChar() to handle backspacing at the start of a line

--- kilo.c
+++ kilo.c
@@ -265,10 +265,17 @@ void editorInsertChar(int c) { // this function doesn't have to worry about wher
 }
 void editorDelChar() {
     if (E.cy == E.numrows) return;
+    if (E.cx == 0 && E.cy == 0) return;
+
     erow *row = &E.row[E.cy];
     if (E.cx > 0) {
         editorRowDelChar(row, E.cx - 1);
         E.cx--;
+    } else {
+        E.cx = E.row[E.cy - 1].size;
+        editorRowAppendString(&E.row[E.cy - 1], row->chars, row->size);
+        editorDelRow(E.cy);
+        E.cy--;
     }
 }
 
