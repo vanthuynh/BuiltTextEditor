1d7432c5d095196308e3acf50842776565484755
Van Huynh <vthuynh@cpp.edu>
2021-12-09 07:56:05 +0000

    Define editorSyntaxToColor() function that maps value in hl to actual color codes

--- kilo.c
+++ kilo.c
@@ -184,6 +184,13 @@ void editorUpdateSyntax(erow *row) {
     }
 }
 
+int editorSyntaxToColor(int hl) {
+    switch (hl) {
+        case HL_NUMBER: return 31;
+        default: return 37;
+    }
+}
+
 /*** row operations ***/
 int editorRowCxToRx(erow *row, int cx) {
     int rx = 0;
